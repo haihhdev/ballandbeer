d43756f8ba643529f4962ea8cc4c4918792571e9
Howf1706 <121178605+Howf1706@users.noreply.github.com>
2025-04-16 19:49:22 +0700

    change product model
    update db, update endpoint

--- services/product/src/routes/productRoutes.js
+++ services/product/src/routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/', productController.getAllProducts);
-router.get('/:id', productController.getProductById);
+router.get('/category/:category', productController.getProductsByCategory); // Lấy sản phẩm theo danh mục
 router.post('/', productController.createProduct);
 router.put('/:id', productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
