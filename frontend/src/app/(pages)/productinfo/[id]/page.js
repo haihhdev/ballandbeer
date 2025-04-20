da6cf8bbcd376fba755cb6bbe7f0faeca27a07df
Hoang Huynh Hai <hhh@gmail.com>
2025-04-20 10:28:08 +0700

    call api product by id , update checkout
    call api product by id , update checkout

---
+++ frontend/src/app/(pages)/productinfo/[id]/page.js
@@ -0,0 +1,10 @@
+import ProductInfo from "@/components/productinfo";
+
+export default function Home() {
+  return (
+    <div>
+      <ProductInfo />
+      {/* Add other components here if needed */}
+    </div>
+  );
+}
