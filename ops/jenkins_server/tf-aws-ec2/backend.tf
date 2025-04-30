3f1b5b79c5056d449e038bbbd9703d773fc77748
Howf1706 <121178605+Howf1706@users.noreply.github.com>
2025-05-01 06:50:29 +0700

    update variables
    update variables

--- ops/jenkins_server/tf-aws-ec2/backend.tf
+++ ops/jenkins_server/tf-aws-ec2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-eks-cicd-1706"
+    bucket = "terraform-eks-cicd-ballandbeer"
     key    = "jenkins/terraform.tfstate"
     region = "us-east-1"
   }
