f99d92e9b3403b0968ed1a1ff3620659f30eaed5
Hoang Huynh Hai <hhh@gmail.com>
2025-05-09 09:27:13 +0700

    update booking v2
    update booking v2

--- frontend/src/app/(pages)/bookinginfo/page.js
+++ frontend/src/app/(pages)/bookinginfo/page.js
@@ -1,6 +1,3 @@
-import Booking from "@/components/booking";
-import Booking3 from "@/components/booking3";
-import Booking2 from "@/components/booking2";
 import Booking4 from "@/components/booking4";
 
 export default function BookingPage() {
