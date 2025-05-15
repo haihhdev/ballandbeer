1402a8bd9fb199a19c28fffd19c1d7d1e340712d
Howf1706 <121178605+Howf1706@users.noreply.github.com>
2025-05-15 20:44:51 +0700

    Update mongodb.js

--- frontend/lib/mongodb.js
+++ frontend/lib/mongodb.js
@@ -1,13 +1,13 @@
 // lib/mongodb.js
 import { MongoClient } from 'mongodb'
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGO_URI
 const options = {}
 
 let client
 let clientPromise
 
-if (!process.env.MONGODB_URI) {
+if (!process.env.MONGO_URI) {
   throw new Error('Please add your Mongo URI to .env.local')
 }
 
