945060729d9132300c492436be3bb9b3cf23acfb
Howf1706 <121178605+Howf1706@users.noreply.github.com>
2025-06-07 20:43:42 +0700

    Update page.js

--- frontend/src/app/(pages)/booking/page.js
+++ frontend/src/app/(pages)/booking/page.js
@@ -1,10 +1,22 @@
+"use client";
 import Booking from "@/components/booking";
+import { useEffect, useRef } from "react";
 
 export default function BookingPage() {
+  const bookingRef = useRef(null);
+
+  useEffect(() => {
+    if (bookingRef.current) {
+      bookingRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return (
     <div>
       {/* Booking Section */}
-      <Booking />
+      <div ref={bookingRef}>
+        <Booking />
+      </div>
     </div>
   );
 }
