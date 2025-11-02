d420a9181717e9ff1ef76e8cbac3ffe20a894f03
Howf1706 <121178605+Howf1706@users.noreply.github.com>
2025-11-02 14:03:40 +0700

    hot fix state file

--- ops/tf-aws-eks/backend.tf
+++ ops/tf-aws-eks/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-ballandbeer-state"
-    key    = "jenkins/terraform.tfstate"
+    key    = "kubernetes/terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
